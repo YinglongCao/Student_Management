df10207c8dbc830dfe5e92886b36ea78186328aa
YinglongCao <18817005523@163.com>
2022-03-29 14:54:14 +0800

    v1.1.2

--- src/main/java/top/ylcao/StdMan/main/Main.java
+++ src/main/java/top/ylcao/StdMan/main/Main.java
@@ -9,12 +9,7 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         OperationMysql opMysql = new OperationMysql("127.0.0.1", 3306, "stdman", "root", "123456");
 
-//        System.out.println(opMysql.searchStudentWithID("B20200505119"));
-
-
         LoginInter loginInter = new LoginInter(opMysql);
-//        System.out.println(opMysql.searchStudentWithName("曹应龙"));
-//        System.out.println(opMysql.searchStudentWithID("B20200505119"));
 
 
 
